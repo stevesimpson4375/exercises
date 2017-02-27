3141131c289772d3c2086db9248201833fdf1229
steve simpson <stevesimpson4375@gmail.com>
2017-02-26 19:23:49 -0500

    Added an example WPF app

---
+++ WpfDemo/WpfDemo/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// http://stackoverflow.com/questions/14500559/create-custom-wpf-event
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Mouse.OverrideCursor = System.Windows.Input.Cursors.Wait;
+            Thread.Sleep(1000);
+            if (!TextBox1.Text.Equals("Hi there"))
+                TextBox1.Text = "Hi there";
+            else TextBox1.Text = "Hi again";
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Mouse.OverrideCursor = System.Windows.Input.Cursors.Arrow;
+        }
+    }
+}
